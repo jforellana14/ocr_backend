bf451cb6fb6aeed1eda2a279bcb9b23d22d52811
Juan Orellana <jforellana14@gmail.com>
2026-05-28 11:21:03 -0600

    Remove OCR dependencies

--- main.py
+++ main.py
@@ -11,8 +11,6 @@ from schemas import DocumentCreate, DocumentResponse
 from fastapi import UploadFile, File
 import shutil
 
-from ocr_utils import process_document
-
 from openpyxl import Workbook
 import os
 
@@ -97,98 +95,3 @@ async def create_manual_document(
 @app.get("/documents", response_model=list[DocumentResponse])
 def get_documents(db: Session = Depends(get_db)):
     return db.query(Document).all()
-
-@app.post("/scan")
-async def scan_document(
-    file: UploadFile = File(...),
-    db: Session = Depends(get_db)
-):
-
-        file_path = f"temp_{file.filename}"
-
-        with open(file_path, "wb") as buffer:
-            shutil.copyfileobj(file.file, buffer)
-
-        extracted_data = process_document(file_path)
-
-        db_document = Document(
-            fecha=extracted_data["fecha"],
-            origen=extracted_data["origen"],
-            destino=extracted_data["destino"],
-            producto=extracted_data["producto"],
-            no_orden_carga=extracted_data["no_orden_carga"],
-            peso_entregado=extracted_data["peso_entregado"],
-            no_constancia_viaje=extracted_data["no_constancia_viaje"],
-
-            raw_text=extracted_data["raw_text"]
-        )
-
-        db.add(db_document)
-        db.commit()
-        db.refresh(db_document)
-
-        return db_document
-
-@app.post("/scan-preview")
-async def scan_preview(
-    file: UploadFile = File(...)
-):
-    file_path = f"temp_{file.filename}"
-
-    with open(file_path, "wb") as buffer:
-        shutil.copyfileobj(file.file, buffer)
-
-    extracted_data = process_document(file_path)
-
-    return extracted_data
-
-@app.get("/export/excel")
-def export_excel(db: Session = Depends(get_db)):
-
-    documents = db.query(Document).all()
-
-    wb = Workbook()
-    ws = wb.active
-    ws.title = "Ordenes"
-
-    headers = [
-        "ID",
-        "Fecha",
-        "Origen",
-        "Destino",
-        "Producto",
-        "Piloto",
-        "No. Orden de Carga",
-        "Peso Entregado",
-        "No. Constancia de Viaje",
-        "Imagen",
-        "Created At"
-    ]
-
-    ws.append(headers)
-
-    for doc in documents:
-        ws.append([
-            doc.id,
-            doc.fecha,
-            doc.origen,
-            doc.destino,
-            doc.producto,
-            doc.piloto,
-            doc.no_orden_carga,
-            doc.peso_entregado,
-            doc.no_constancia_viaje,
-            doc.image_path,
-            str(doc.created_at)
-        ])
-
-    os.makedirs("exports", exist_ok=True)
-
-    file_path = "exports/documents.xlsx"
-    wb.save(file_path)
-
-    return FileResponse(
-        path=file_path,
-        filename="documents.xlsx",
-        media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-    )
