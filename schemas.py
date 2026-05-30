e0f4c68598b86ceefc18e073fe8fbf4981e397d7
Juan Orellana <jforellana14@gmail.com>
2026-05-29 21:59:37 -0600

    Add document CRUD and pilots list

--- schemas.py
+++ schemas.py
@@ -15,6 +15,15 @@ class DocumentCreate(BaseModel):
     piloto: Optional[str] = None
     image_path: Optional[str] = None
 
+class DocumentUpdate(BaseModel):
+    fecha: Optional[str] = None
+    origen: Optional[str] = None
+    destino: Optional[str] = None
+    producto: Optional[str] = None
+    piloto: Optional[str] = None
+    no_orden_carga: Optional[str] = None
+    peso_entregado: Optional[str] = None
+    no_constancia_viaje: Optional[str] = None
 
 class DocumentResponse(DocumentCreate):
     id: int
